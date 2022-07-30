ce3e840deb49cf79fea401985d472b136c262fd2
Ricardo J. Serrano <rserran1@gmail.com>
2022-07-30 19:34:06 -0400

    Add Chapter 8 - Exponential Smoothing markdown file and knitr output (work in progress)

--- ch_8/ch_8_examples.R
+++ ch_8/ch_8_examples.R
@@ -6,6 +6,7 @@
 suppressMessages(library(tidyverse))
 library(fpp3)
 library(plotly)
+theme_set(theme_minimal())
 
 # algeria_economy
 algeria_economy <- global_economy %>%
@@ -13,7 +14,8 @@ algeria_economy <- global_economy %>%
 
 algeria_economy %>%
      autoplot(Exports) + 
-     theme_minimal() + 
+     geom_smooth(method = 'loess', se = FALSE, color = 'blue') + 
+     expand_limits(y = c(0, 60)) + 
      labs(y = "% of GDP", title = "Exports: Algeria")
 
 # STL decomposition method
@@ -28,3 +30,23 @@ components(dcmp) %>%
 
 # Comments: Time series does not have a seasonal component. No obvious trend
 # pattern is illustrated.
+
+# Example: Algerian exports
+
+# Estimate parameters
+fit <- algeria_economy %>%
+     model(ETS(Exports ~ error("A") + trend("N") + season("N")))
+
+fc <- fit %>%
+     forecast(h = 5)
+
+fc
+
+# plot forecast
+fc %>%
+     autoplot(algeria_economy) +
+     geom_line(aes(y = .fitted), col="#D55E00",
+               data = augment(fit)) +
+     labs(y="% of GDP", title="Exports: Algeria") +
+     guides(colour = "none")
+
